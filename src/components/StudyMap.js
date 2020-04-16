9820fd54740d9b719364370fb3de90cd93d2efe4
ym85jo <ehli@nate.com>
2020-04-16 13:19:24 +0900

    Update StudyMap.js

--- src/components/StudyMap.js
+++ src/components/StudyMap.js
@@ -51,32 +51,32 @@ export default function () {
                         image: markerImage // 마커 이미지
                     });
                 }
-            });
-            
-            
-            
-            var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
-            var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
-            var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+                
+                
+                var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
+                var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
+                var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-            var position1 = new kakao.maps.LatLng(33.450701, 126.570667);
+                var position1 = new kakao.maps.LatLng(33.450701, 126.570667);
 
-            // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
-            roadviewClient.getNearestPanoId(position1, 50, function(panoId) {
-                roadview.setPanoId(panoId, position1); //panoId와 중심좌표를 통해 로드뷰 실행
+                // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
+                roadviewClient.getNearestPanoId(position1, 50, function(panoId) {
+                    roadview.setPanoId(panoId, position1); //panoId와 중심좌표를 통해 로드뷰 실행
+                });
+                
+                
             });
-            
-            
-            
-            
         }
     }, []);
 
     return (
         <Block>
             <Section h1="Map API" h2="광명시 지역화폐 사용처" mode="">
+        
+                <div id={`roadview`} style={{ width: "1000px", height: "600px" }} />        
+                <br/>
                 <div id={`map`} style={{ width: "1000px", height: "600px" }} />
-                <div id={`roadview`} style={{ width: "1000px", height: "600px" }} />
+
             </Section>
         </Block>
     );
